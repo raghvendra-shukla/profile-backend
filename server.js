867a2dbd2995be8462ccaa87a54b7860d322237d
Raghvendra <rannyshukla762@gmail.com>
2022-09-26 17:47:23 +0530

    modify

--- server.js
+++ server.js
@@ -4,11 +4,7 @@ const express = require('express');
 const cors =require("cors");
 const app = express();
 const port =process.env.PORT || 5000;
-
-app.get("/",(req,res)=>{
-  res.render("index");
-})
-
+app.set("view engine","ejs");
 //A middleWare to  use req.body
 app.use(express.json());
 app.use(
@@ -16,9 +12,11 @@ app.use(
     origin:"*"
   })
 )
-app.set("view engine","ejs");
 app.use(express.static(__dirname + '/public'));
 app.use('/public', express.static(__dirname + '/public'));
+app.get("/",(req,res)=>{
+  res.render("index");
+})
 //Avialable Routes
 app.use("/api/auth",require("./routes/auth"));
 app.use("/api/profile",require("./routes/profile"));
